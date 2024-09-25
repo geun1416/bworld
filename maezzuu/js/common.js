4915ae58b3ac49f5d6e8a35a85ced0d2b530d97a
maezzuu <o3o_hell0@naver.com>
2024-09-26 03:33:25 +0900

    header html css javascript

--- maezzuu/js/common.js
+++ maezzuu/js/common.js
@@ -1,62 +1,39 @@
+$(function () {
+  $('.gnb_list').on('mouseover', function () {
+    $('.gnb_list').removeClass('current');
+    $('.gnb_menu').removeClass('currenton').stop(true, true).slideUp();
 
-$(function() {
-   AOS.init();
-});
+    $(this).addClass('current');
 
-
-$(function() {
-  if ($("header").length) {
-    $(window).scroll(function() {
-      let scrollTop = $(document).scrollTop();
-      if (scrollTop > 800) {
-        $('header').addClass('fixed');
-      } else {
-        $('header').removeClass('fixed');
-      }
-    });
-  }
-});
-
-
-$(function() { 
-  $('.topBtn').css('display','none');
-  $(window).scroll(function() { if ($(this).scrollTop() > 300) { 
-    $('.topBtn').fadeIn();
-      } else { 
-        $('.topBtn').fadeOut(); 
-      } 
+    const index = $(this).index();
+    $('.gnb_menu').eq(index).addClass('currenton').stop(true, true).slideDown();
   });
-	$('.topBtn').click(function(){
-		$('html, body').animate({scrollTop:0},500);
-	});
+
+
+  $('.gnb_menu').on('mouseenter', function () {
+    const index = $('.gnb_menu').index($(this));
+    $('.gnb_list').eq(index).addClass('current');
+  });
+
+  
+  $('.gnb_list').on('mouseleave', function () {
+    $(this).addClass('current');
+  });
+  $('.gnb_menu').on('mouseleave', function () {
+    const index = $('.gnb_menu').index($(this));
+    $('.gnb_menu').eq(index).removeClass('currenton').stop(true, true).slideUp();
+    $('.gnb_list').eq(index).removeClass('current');
+  });
 });
 
 
-// $(function() {
-//   $(".menu ul li").on("mouseenter", function() {
-//     $(this).addClass("on");
-//   });
-// });
-// $(function() {
-//   $(".menu ul li").on("mouseleave", function() {
-//     $(this).removeClass("on");
-//   });
-// });
 
+$(function(){
+  $('.toggle').on('click',function(){
+      const $dep3 = $(this).children('.dep3');
 
-$(function() {
-  var mySwiper = new Swiper('.meimg_slide', {
-    slidesPerView: 1, // 한 번에 표시할 슬라이드 수
-    loop: true, // 슬라이드 루프(무한 회전) 활성화
-    autoplay: {
-        delay: 2000, // 2초마다 자동 재생
-        disableOnInteraction: false // 사용자 상호 작용 후에도 자동 재생 유지
-    },
-    effect: 'fade', // 페이드 인/아웃 효과
-    direction: 'horizontal', // 슬라이드 방향 (수평)
-    touchRatio: 1, // 슬라이드 드래그 감도
-    mousewheel: false, // 마우스 휠로 슬라이드 이동 가능
-    centeredSlides: true, // 슬라이드 중앙 정렬
-    watchOverflow: true // 슬라이드가 화면을 넘어갈 때의 처리 설정
+      $('.dep3').not($dep3).removeClass('on').slideUp();
+
+      $dep3.toggleClass('on').slideToggle();
   });
 });
