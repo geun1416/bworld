acb1d1de63ad9ed643e0db8002ba65f108e4e3ec
maezzuu <o3o_hell0@naver.com>
2024-09-30 18:30:28 +0900

    main컨텐츠 추가

--- maezzuu/js/common.js
+++ maezzuu/js/common.js
@@ -1,6 +1,20 @@
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
+  loop: true, 
+  pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
-    },
-  });
+  },
+  navigation: {
+      nextEl: ".swiper-button-next", 
+      prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".suggestSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
