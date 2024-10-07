efcfc20a1b4233d9330df2a750956a1ba63eb36b
geun <1416geun@naver.com>
2024-10-07 17:14:16 +0900

    .

--- geun/js/common.js
+++ geun/js/common.js
@@ -7,3 +7,24 @@
         },
       });
  });
+
+ $(function() {
+  var swiper = new Swiper(".productsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      780: {
+        slidesPerView: 1, 
+        spaceBetween: 0,
+      },
+    },
+  });
+});
